e5dfef87db42b490c03a08a41fe7f0990bf92d23
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-07-13 18:23:11 +0530

    Update __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -1,4 +1,9 @@
 # Credit - adarsh-goel
+#• © TonyStarkBotz
+
+# Don't Remove Credit @TonyStark_Botz
+# Join our Telegram Channel For Amazing Bot @TonyStark_Botz
+# Ask Doubt on telegram @TonyStarkBotzXSupport
 
 from aiohttp import web
 from web.stream_routes import routes
@@ -6,3 +11,9 @@ from web.stream_routes import routes
 
 web_app = web.Application()
 web_app.add_routes(routes)
+#• © TonyStarkBotz
+
+# Don't Remove Credit @TonyStark_Botz
+# Join our Telegram Channel For Amazing Bot @TonyStark_Botz
+# Ask Doubt on telegram @TonyStarkBotzXSupport
+
